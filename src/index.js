7d4b36fa2f6df53fbfce582c1f1c629ec1a6f71c
bborelli <borelli.bruna@gmail.com>
2024-11-04 20:45:07 +0000

    Added the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("city");
+
+  cityElement.innetHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "4fbe9b2c44d0f8a0833d1te403cbb78o";
+  let upiUrl = `https://api.shecodes.io/weather/v1/current?query${city}&key${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("city");
-  cityElement.innetHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
